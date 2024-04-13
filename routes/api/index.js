89f7b1eec687043aae313cc7c5014f7aa040d697
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-04-13 21:02:48 +0100

    Added route for live status

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+router.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: 'OK',
+  });
+});
+
 module.exports = router;
