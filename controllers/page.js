89f267e90b65c50b6757cfab7f27df7e2964508f
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-04-16 20:59:15 +0100

    Created getPage, getPages and removeage

--- controllers/page.js
+++ controllers/page.js
@@ -39,4 +39,85 @@ async function createPage(req, res) {
   }
 }
 
-module.exports = { createPage };
+async function getPage(req, res) {
+  const { id } = req.params;
+
+  try {
+    const page = await Page.findById(id).populate('website');
+
+    if (!page) {
+      return res.status(404).json({
+        success: false,
+        message: 'Page not found',
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      page,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
+
+async function getPages(req, res) {
+  try {
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 10;
+    const sort = req.query.sort || 'createdAt';
+    const sortDirection = req.query.sortDirection === 'desc' ? -1 : 1;
+
+    const { websiteId } = req.query;
+
+    const options = {
+      limit: limit,
+      sort: { [sort]: sortDirection },
+    };
+
+    const pages = await Page.paginate(
+      websiteId ? { website: websiteId } : {},
+      options,
+    );
+
+    return res.status(200).json({
+      success: true,
+      pages,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
+
+async function removePage(req, res) {
+  const { id } = req.params;
+
+  try {
+    const page = await Page.findByIdAndDelete(id);
+
+    if (!page) {
+      return res.status(404).json({
+        success: false,
+        message: 'Page not found',
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      page,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
+
+module.exports = { createPage, getPage, getPages };
