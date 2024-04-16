9597ddbb6d84acf9c42abb32aad290d08ce4716c
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-04-16 11:03:18 +0100

    Created controller for adding a website

---
+++ controllers/websites.js
@@ -0,0 +1,30 @@
+const Website = require('../models/Website');
+const { validateWebsite } = require('../utils/validation/website');
+
+async function createWebsite(req, res) {
+  const { url } = req.body;
+
+  try {
+    const website = { url };
+    const { error } = validateWebsite(website);
+
+    if (error) {
+      return res.status(400).json({
+        success: false,
+        errors: error.details.map((error) => error.message),
+      });
+    }
+
+    const newWebsite = await Website.create(website);
+
+    return res.status(201).json({
+      success: true,
+      website,
+    });
+  } catch (error) {
+    return res.status(400).json({
+      success: false,
+      message: error.message,
+    });
+  }
+}
