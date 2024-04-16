ba4db91bc08ab9f166df0e1461b6fc938886ce11
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-04-17 00:46:25 +0100

    Installed PrimeNG and TailwindCSS

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+
+  items: MenuItem[]; // Define menu items array
+
+  constructor() {
+    this.items = [];
+  }
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        icon: 'pi pi-fw pi-home',
+        routerLink: ['/']
+      },
+    ];
+  }
 }
