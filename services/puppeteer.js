bcc183ab3f9a7eb1c12838cf42374589823a3135
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-04-23 14:40:04 +0100

    Created puppeteer service to take websites screenshots

---
+++ services/puppeteer.js
@@ -0,0 +1,21 @@
+const puppeteer = require('puppeteer');
+
+async function binaryScreenshot(url) {
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setViewport({
+      width: 1728,
+      height: 940,
+      deviceScaleFactor: 1,
+    });
+    await page.goto(url, { waitUntil: 'networkidle2' });
+
+    return await page.screenshot({ encoding: 'binary' });
+  } catch (error) {
+    console.log(error.message);
+    return null;
+  }
+}
+
+module.exports = { binaryScreenshot };
