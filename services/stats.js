144251a1c37d78f6769281c6a316e4c1e9fe73e1
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-05-04 02:00:12 +0100

    Save top 10 errors

--- services/stats.js
+++ services/stats.js
@@ -1,6 +1,8 @@
 // Service to update the stats of a website
 
 const Page = require('../models/Page');
+const PageEvaluation = require('../models/PageEvaluation');
+const mongoose = require('mongoose');
 const { notifyWebsiteUpdate } = require('./sockets');
 
 async function updateStats(website) {
@@ -21,6 +23,7 @@ async function updateStats(website) {
     pagesWithAAAErrors: evaluatedPages.filter((p) => p.stats.hasAAAErrors)
       .length,
     evaluatedPages: evaluatedPages.length,
+    topErrors: await getTop10Errors(website),
   };
 
   website.stats = { ...newStats };
@@ -42,6 +45,65 @@ async function updateStats(website) {
   await website.save();
 
   return website;
+}
+
+async function getTop10Errors(website) {
+  const failedAssertions = await PageEvaluation.aggregate([
+    {
+      $match: {
+        website: website._id,
+        // actRules.assertions array must have elements with failed in outcome field
+      },
+    },
+    {
+      $project: {
+        'actRules.assertions': {
+          $filter: {
+            input: '$actRules.assertions',
+            as: 'assertion',
+            cond: { $eq: ['$$assertion.metadata.outcome', 'failed'] },
+          },
+        },
+        'wcagTechniques.assertions': {
+          $filter: {
+            input: '$wcagTechniques.assertions',
+            as: 'assertion',
+            cond: { $eq: ['$$assertion.metadata.outcome', 'failed'] },
+          },
+        },
+      },
+    },
+  ]);
+
+  let actConcat = [];
+  let wcagConcat = [];
+
+  for (const pageEval of failedAssertions) {
+    actConcat = actConcat.concat(pageEval.actRules.assertions);
+    wcagConcat = wcagConcat.concat(pageEval.wcagTechniques.assertions);
+  }
+
+  const allConcat = actConcat.concat(wcagConcat);
+
+  // Find the top 10 most common errors and return them with the count, the name, the code and the description
+  const assertionCounts = allConcat.reduce((acc, assertion) => {
+    acc[assertion.code] = (acc[assertion.code] || 0) + 1;
+    return acc;
+  }, {});
+
+  const topErrors = Object.keys(assertionCounts)
+    .sort((a, b) => assertionCounts[b] - assertionCounts[a])
+    .slice(0, 10)
+    .map((code) => {
+      const assertion = allConcat.find((a) => a.code === code);
+      return {
+        code,
+        description: assertion.description,
+        count: assertionCounts[code],
+      };
+    });
+
+  return topErrors;
 }
 
 module.exports = { updateStats };
