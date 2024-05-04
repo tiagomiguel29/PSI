046c40ef3a9d46f2372d837cb0e8b9af8566c983
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-05-04 01:48:46 +0100

    Added website ref and field specs for act and wcag

--- models/PageEvaluation.js
+++ models/PageEvaluation.js
@@ -1,3 +1,4 @@
+const { any } = require('joi');
 const mongoose = require('mongoose');
 
 const PageEvaluationSchema = new mongoose.Schema(
@@ -5,6 +6,11 @@ const PageEvaluationSchema = new mongoose.Schema(
     page: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'Page',
+      required: true,
+    },
+    website: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Website',
       required: true,
     },
     result: {
@@ -20,21 +26,46 @@ const PageEvaluationSchema = new mongoose.Schema(
       warning: Number,
       failed: Number,
       inapplicable: Number,
-      assertions: Object,
+      assertions: [
+        {
+          name: String,
+          code: String,
+          mapping: String,
+          description: String,
+          metadata: {
+            'success-criteria': Array,
+            passed: Number,
+            warning: Number,
+            failed: Number,
+            inapplicable: Number,
+            outcome: String,
+            description: String,
+          },
+        },
+      ],
     },
     wcagTechniques: {
       passed: Number,
       warning: Number,
       failed: Number,
       inapplicable: Number,
-      assertions: Object,
-    },
-    bestPractices: {
-      passed: Number,
-      warning: Number,
-      failed: Number,
-      inapplicable: Number,
-      assertions: Object,
+      assertions: [
+        {
+          name: String,
+          code: String,
+          mapping: String,
+          description: String,
+          metadata: {
+            'success-criteria': Array,
+            passed: Number,
+            warning: Number,
+            failed: Number,
+            inapplicable: Number,
+            outcome: String,
+            description: String,
+          },
+        },
+      ],
     },
   },
   {
