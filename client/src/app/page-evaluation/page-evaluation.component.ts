2e293a30f713c236e0f713363c0c0c4353ce30f4
Tiago Miguel <66640308+tiagomiguel29@users.noreply.github.com>
2024-05-23 20:01:52 +0100

    Made ui for stats and filters

--- client/src/app/page-evaluation/page-evaluation.component.ts
+++ client/src/app/page-evaluation/page-evaluation.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Page } from 'src/types/Page';
+import { PagesService } from '../services/pages.service';
+import { ActivatedRoute } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-page-evaluation',
@@ -6,5 +11,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./page-evaluation.component.scss']
 })
 export class PageEvaluationComponent {
+  pageId: string;
+  evaluation: any;
+  page?: Page;
+  isLoading = true;
+  rules = this._formBuilder.group({
+    act: true,
+    wcag: true,
+  });
+  results = this._formBuilder.group({
+    passed: true,
+    failed: true,
+    inapplicable: true,
+    warning: true,
+  });
+  levels = this._formBuilder.group({
+    a: true,
+    aa: true,
+    aaa: true,
+  });
+
+  constructor(
+    private pagesService: PagesService,
+    private route: ActivatedRoute,
+    private messageService: MessageService,
+    private _formBuilder: FormBuilder,
+  ) {
+    this.pageId = this.route.snapshot.paramMap.get('id')!;
+  }
+
+  ngOnInit() {
+    this.fetchEvaluation();
+  }
+
+  fetchEvaluation() {
+    this.pagesService.getPageEvaluation(this.pageId).subscribe({
+      next: response => {
+        if (response.success) {
+          this.evaluation = response.evaluation;
+          this.page = response.page;
+        } else {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Error',
+            detail: response.message || 'An error occurred fetching the evaluation.',
+          });
+        }
+        this.isLoading = false;
+      },
+      error: error => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: 'An error occurred fetching the evaluation.',
+        });
+        this.isLoading = false;
+      }
+    });
+  }
 
 }
